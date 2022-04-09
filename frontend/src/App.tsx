b8bf1ae0a8949288fc957567998a7d9a971ae051
wilson dennison <wilson.dennison@gmail.com>
2022-04-09 20:12:56 +0200

    frontend: cra rip out. fresh index.tsx to get started

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
-class App extends React.Component {
 
-    render() {
-        return <div>I hate Paul the Planner</div>
+export class App extends React.Component {
+    render()  {
+        return <div>Fuck you Paul the Planner from my App</div> 
     }
 }
-
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
