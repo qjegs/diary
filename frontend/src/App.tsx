a476c71aaa1eed099e84c021d129aaa0b6eae1bd
wilson dennison <wilson.dennison@gmail.com>
2022-04-14 16:58:24 -0700

    updating directory locations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Sidebar } from "./Sidebar";
+import { Sidebar } from "./components/Sidebar";
 
 
 export class App extends React.Component {
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from './sidebar.module.css';
+import styles from './css/sidebar.module.css';
 
 
 export class Sidebar extends React.Component {
