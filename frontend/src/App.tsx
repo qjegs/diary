deaed31814f3fa162ae883d33233dfdd48ade07f
wilson dennison <wilson.dennison@gmail.com>
2022-04-14 16:51:42 -0700

    Starting side bar and adding some simple css.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Sidebar } from "./Sidebar";
 
 
 export class App extends React.Component {
     render()  {
-        return <div>Fuck you Paul the Planner from my App</div> 
+        return <div>
+                    <nav>
+                        <Sidebar />
+                    </nav>
+                Fuck you Paul the Planner from my App
+                </div> 
     }
 }
---
+++ frontend/src/Sidebar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from './sidebar.module.css';
+
+
+export class Sidebar extends React.Component {
+
+    OnNewItemClick(): void {
+        alert('New Item');
+    }
+    render() : JSX.Element {
+        return (
+        <div>
+           
+                <nav>
+                    <ul>
+                        <li ><button onClick={this.OnNewItemClick}>New Item</button></li>
+                        <li className={styles.listitem}>Edit</li>
+                        <li>Delete</li>                
+                    </ul>
+                </nav>
+            
+        </div>
+            
+                   )
+    }
+}
